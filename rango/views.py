a6965ac1e16de0d9177b5de878ce0e2c0afd359a
Erik Veloso <erikvelosorj@gmail.com>
2017-01-29 23:47:39 -0200

    Up to exercises before Form chapter.

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rango.models import Category
+from rango.models import Page
 
 
 def index(request):
@@ -11,5 +12,25 @@ def index(request):
 
 
 def about(request):
+
     context_dict = {'message': 'Enjoy!'}
     return render(request, 'rango/about.html', context=context_dict)
+
+
+def show_category(request, category_name_slug):
+    
+    context_dict = {}
+    
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+        
+    except Category.DoesNotExist:
+        context_dict['pages'] = None
+        context_dict['category'] = None
+    
+    return render(request, 'rango/category.html', context_dict)
+
+
